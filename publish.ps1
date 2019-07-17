b2f6a3f033e3afd847adf3489b7f4a1f3ace4349
treyhay31 <treyhay@gmail.com>
2019-07-17 19:32:08 -0400

    adding publish and updating pipeline

---
+++ publish.ps1
@@ -0,0 +1,14 @@
+$publishModuleSplat = @{
+    Path        = ".\ReleaseMe\GG"
+    NuGetApiKey = $ENV:nugetapikey
+    Verbose     = $true
+    Force       = $true
+    Repository  = "PSGallery"
+    ErrorAction = 'Stop'
+}
+
+"Files in module output:"
+Get-ChildItem ".\ReleaseMe\GG" -Recurse -File |
+    Select-Object -Expand FullName
+
+Publish-Module @publishModuleSplat
